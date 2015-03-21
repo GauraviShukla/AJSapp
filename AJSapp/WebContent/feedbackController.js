ba72cff9621bfb7e3569bc58eb9575d1f1ec37b0
Gauravi Shukla <gauravi.shukla@sap.com>
2015-03-21 22:56:52 +0530

    stable app
    Change-Id: I7bd869c81c9d989d6724f381f1acc1cb850f276e
    Signed-off-by: Gauravi Shukla <gauravi.shukla@sap.com>

--- AJSapp/WebContent/feedbackController.js
+++ AJSapp/WebContent/feedbackController.js
@@ -10,20 +10,6 @@
 				 tdArray.push(tdArrayObj);
 			 }
 			 $scope.feedback = response;
-//			 $scope.appendStars =function(){
-//				 for(var i=0; i< tdArray.length; i++){
-//					 var parent = document.getElementById(tdArray[i].parent);
-//					 if(parent){
-//						 $('#'+tdArray[i].parent).empty();
-//						 for(var j=0; j<tdArray[i].value; j++){
-//							 var element = document.createElement('span');
-//							 element.setAttribute("class","glyphicon glyphicon-star");
-//							 parent.appendChild(element);
-//						 }
-//					 }
-//				 }
-//				 return ;
-//			 } 
 			 
 			 $scope.overallrating = function(){
 				 var sum = 0;
@@ -35,30 +21,39 @@
 	 });
 	 $scope.rate =7;
 	 $scope.ratingOptions = [{value: "Strongly agree"}, {value: "Agree" }, { value: "Neutral"}, { value: "Disagree"}, { value: "Strongly disagree"}];
-	 $scope.MCQs = [ { index: 1, ques: "The website is easy to navigate"}, 
-	                 { index: 2, ques: "Content are displayed properly across the website"}, 
-	                 { index: 3, ques: "I found the website professional"}];
-	 $scope.MCQA1 =0;
-	 $scope.MCQA2 =0;
-	 $scope.MCQA3 =0;
+	 $scope.MCQs = [ { index: 1, ques: "The website is easy to navigate", ans:0, MCQoptions:[{value:5}, {value:4}, {value:3}, {value:2},{value:1}]}, 
+	                 { index: 2, ques: "Content are displayed properly across the website", ans:0, MCQoptions:[{value:5}, {value:4}, {value:3}, {value:2},{value:1}]}, 
+	                 { index: 3, ques: "I found the website professional", ans:0, MCQoptions:[{value:5}, {value:4}, {value:3}, {value:2},{value:1}]}];
 	 $scope.Questions =[
-	                    {no: "Q1", Ques: "Did you find what you needed?", answers:[
-	                                                                     { answer: "Yes, all of it"},
-	                                                                     { answer: "Yes, some of it"},
-	                                                                     { answer: "No, none of it"}
+	                    {no: "Q1", Ques: "Did you find what you needed?", ans:0, options:[
+	                                                                     { option: "Yes, all of it", value: 5},
+	                                                                     { option: "Yes, some of it", value: 3},
+	                                                                     { option: "No, none of it", value: 1}
 	                    												]},
-	                    {no: "Q2", Ques: "Please tell us how easy it is to find information on the site.", answers:[
-	                                                                     { answer: "Very easy"},
-	                                                                     { answer: "Easy"},
-	                                                                     { answer: "Average"},
-	                                                                     { answer: "Difficult"},
-	                                                                     { answer: "Very difficult"}
-	                    												]},		
+	                    {no: "Q2", Ques: "Please tell us how easy it is to find information on the site.", ans:0, options:[
+											                                                                     { option: "Very easy", value: 5},
+											                                                                     { option: "Easy" , value:4},
+											                                                                     { option: "Average", value: 3},
+											                                                                     { option: "Difficult", value: 2},
+											                                                                     { option: "Very difficult", value: 1}
+											                                                                 ]},		
 	                    {no: "Q3", Ques: "Mention any features or functionalities that you think we should add on the website"}											
 	                 ];
-//	 $scope.calculateAvg = function(){
-//		 alert("id");
-//	 }
+	 $scope.submitValue = 0;
+	 $scope.calculate = function(){
+		 var sum = 0;
+		 var counter =0;
+		 for(var i=0; i<$scope.MCQs.length; i++){
+			 sum = sum + $scope.MCQs[i].ans ;
+			 counter++;
+		 }
+		 for(var i=0; i<$scope.Questions.length; i++){
+			 sum = sum + $scope.MCQs[i].ans ;
+			 counter++;
+		 }
+		 $scope.submitValue = sum/counter;
+		 
+	 }
 
  });
  
